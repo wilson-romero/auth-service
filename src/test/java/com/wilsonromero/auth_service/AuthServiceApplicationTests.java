b8ccaa16581d20f5c9d86cf367576b4fd74df2b2
Wilson Romero <me@wilsonromero.com>
2024-08-07 15:29:21 -0500

    :sparkles:: spring initializr

---
+++ src/test/java/com/wilsonromero/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonromero.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
